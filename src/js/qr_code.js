e3121777c331d9d6a45d04e4d7df9f0fd9c2b81c
Alberson Miranda <albersonmiranda@hotmail.com>
2024-12-31 02:51:11 -0300

    add itemName to transactionID

--- src/js/qr_code.js
+++ src/js/qr_code.js
@@ -87,7 +87,7 @@ function addBuyButtonListeners() {
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
-        return `casamentoRA${year}${month}${day}${hours}${minutes}${seconds}`;
+        return `casamentoRA${itemName}${year}${month}${day}${hours}${minutes}`;
       };
 
       const pixParams = {
